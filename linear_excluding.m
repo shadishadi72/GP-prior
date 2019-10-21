da8e3c751f6cd1fd83035ddd5e2cb4236f45091c
shadishadi72 <38002675+shadishadi72@users.noreply.github.com>
2019-10-21 18:06:46 +0100

    mean function that allows us to select features

---
+++ linear_excluding.m
@@ -0,0 +1,11 @@
+function [m,dm] = linear_excluding(idxs,hyp,x)
+if nargin<3, m = 'D'; return; end             % report number of hyperparameters
+
+%hyp = varargin{1};
+%x = varargin{2};
+hyp(idxs) = 0;
+m = x * hyp(:);
+
+dm =  @(q) x'*q(:);
+
+end
